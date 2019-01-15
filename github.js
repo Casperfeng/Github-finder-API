1f0b08280eef6dc38eecabac3a3c96a28d6261ec
Casperfeng <casp.feng@gmail.com>
2019-01-15 20:37:42 +0100

    now displays repos

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class Github {
   constructor() {
     this.client_id = 'e69334fbe3b7a47ec010';
     this.client_secret = 'f71ab3f99d3e6875ebbbfc940b25fef536e9989d';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
--- ui.js
+++ ui.js
@@ -3,7 +3,7 @@ class UI {
     this.profile = document.getElementById('profile');
   }
 
-  // Display profile UI
+  // Display profile in UI
   showProfile(user) {
     this.profile.innerHTML = `
       <div class="card card-body mb-3">
@@ -32,8 +32,8 @@ class UI {
     `;
   }
 
-   // Show user repos
-   showRepos(repos) {
+  // Show user repos
+  showRepos(repos) {
     let output = '';
 
     repos.forEach(function(repo) {
@@ -53,37 +53,44 @@ class UI {
       `;
     });
 
+    // Output repos
+    document.getElementById('repos').innerHTML = output;
+  }
+
   // Show alert message
-  showAlert(msg, className ){
-    //Clear any remaining alerts
+  showAlert(message, className) {
+    // Clear any remaining alerts
     this.clearAlert();
     // Create div
-    const div = document.createElement("div");
-    // Add class
+    const div  =  document.createElement('div');
+    // Add classes
     div.className = className;
     // Add text
-    div.appendChild(document.createTextNode(msg));
+    div.appendChild(document.createTextNode(message));
     // Get parent
-    const container = document.querySelector(".searchContainer");
+    const container =  document.querySelector('.searchContainer');
     // Get search box
-    const search = document.querySelector(".search");
+    const search = document.querySelector('.search');
     // Insert alert
     container.insertBefore(div, search);
+
+    // Timeout after 3 sec
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
   }
 
   // Clear alert message
   clearAlert() {
-    const currentAlert = document.querySelector(".alert");
+    const currentAlert = document.querySelector('.alert');
 
-    if (currentAlert){
+    if(currentAlert){
       currentAlert.remove();
     }
-    // Clear after 3 seconds
-    setTimeout(() => this.clearAlert(), 3000);
   }
 
-  // Clears profile
+  // Clear profile
   clearProfile() {
-    this.profile.innerHTML = "";
+    this.profile.innerHTML = '';
   }
 }
