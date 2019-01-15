cd02945491be779e75e9611e175254eca9748351
Casperfeng <casp.feng@gmail.com>
2019-01-15 18:49:54 +0100

    added listener to searchfield

---
+++ app.js
@@ -0,0 +1,13 @@
+// Search input 
+const seachUser = document.getElementById("searchUser");
+
+// Search input event listener
+searchUser.addEventListener("keyup", (e) => {
+  // Get input text
+  const userText = e.target.value;
+
+
+  if (userText !== ""){
+  }
+
+});
