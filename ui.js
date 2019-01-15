d29b294bec57b375352ddb6b427d1e9bdde86a9f
Casperfeng <casp.feng@gmail.com>
2019-01-15 20:22:22 +0100

    now clears error alert, displays one error at the time

--- ui.js
+++ ui.js
@@ -34,6 +34,8 @@ class UI {
 
   // Show alert message
   showAlert(msg, className ){
+    //Clear any remaining alerts
+    this.clearAlert();
     // Create div
     const div = document.createElement("div");
     // Add class
@@ -46,6 +48,15 @@ class UI {
     const search = document.querySelector(".search");
     // Insert alert
     container.insertBefore(div, search);
+  }
+
+  // Clear alert message
+  clearAlert() {
+    const currentAlert = document.querySelector(".alert");
+
+    if (currentAlert){
+      currentAlert.remove();
+    }
   }
 
   // Clears profile
