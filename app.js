9e6b29c1a6ccee5584cb73a14980ffc215340a7e
Casperfeng <casp.feng@gmail.com>
2019-01-15 19:56:57 +0100

    now displays picture and information upon search

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
 // Init Github
-const github = new GitHub;
-
-//Init UI
+const github = new Github;
+// Init UI
 const ui = new UI;
 
-// Search input 
-const seachUser = document.getElementById("searchUser");
+// Search input
+const searchUser = document.getElementById('searchUser');
 
 // Search input event listener
-searchUser.addEventListener("keyup", (e) => {
+searchUser.addEventListener('keyup', (e) => {
   // Get input text
   const userText = e.target.value;
 
-
-  if (userText !== ""){
-    // Make http call
-    github.getUser(userText).then(data => {
-      if (data.profile.message === "Not Found") {
+  if(userText !== ''){
+   // Make http call
+   github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
         // Show alert
-      
+
       } else {
         // Show profile
         ui.showProfile(data.profile);
-
       }
     })
-  }
-
-  else {
+  } else {
     // Clear profile
-
+    
   }
-
-});
+}); 
--- github.js
+++ github.js
@@ -1,14 +1,13 @@
-class GitHub {
-  constructor(){
-    this.client_id = "e69334fbe3b7a47ec010";
-    this.client_secret = "f71ab3f99d3e6875ebbbfc940b25fef536e9989d";
+class Github {
+  constructor() {
+    this.client_id = 'e69334fbe3b7a47ec010';
+    this.client_secret = 'f71ab3f99d3e6875ebbbfc940b25fef536e9989d';
   }
 
   async getUser(user) {
-    const profileResponse = await fetch(`
-    https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-    const profileData =await profileResponse.json();
+    const profile = await profileResponse.json();
 
     return {
       profile
